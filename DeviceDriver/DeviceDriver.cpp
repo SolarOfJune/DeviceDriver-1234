82f5f7e848bf714a44eef2e46190a2bf516b4e4f
jungi.lee <dffbnm@gmail.com>
2023-07-07 11:17:55 +0900

    [feature] DeviceDriver: read
    Signed-off-by: jungi.lee <dffbnm@gmail.com>

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,22 @@
+#include <stdexcept>
+#include <windows.h>
 #include "DeviceDriver.h"
+
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int read_value = (int)(m_hardware->read(address));
+    for(int i = 0; i < READ_CNT - 1; i++)
+    {
+        Sleep(DELAY_TIME);
+        if (read_value != (int)(m_hardware->read(address)))
+            throw exception("ReadFailException");
+    }
+    return read_value;
 }
 
 void DeviceDriver::write(long address, int data)
